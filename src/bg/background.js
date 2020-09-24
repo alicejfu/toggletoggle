69d62c814cc4e2d0f72fa677abd412d9386c5bca
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-09-24 00:46:59 -0400

    implemented saved and load css styles

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,13 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// chrome.extension.onMessage.addListener(function (
+chrome.extension.onMessage.addListener(function (
+  request,
+  sender,
+  sendResponse
+) {
+  // chrome.pageAction.show(sender.tab.id);
+  sendResponse();
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,55 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      console.log('Hello. This message was sent from scripts/inject.js');
+      // ----------------------------------------------------------
+      // alert('alert from inject.js');
+      const cssObj = [];
 
-	}
-	}, 10);
+      const newStyle = document.createElement('style');
+      newStyle.setAttribute('id', 'toggle-toggle');
+      newStyle.setAttribute('type', 'text/css');
+
+      document.querySelector('head').appendChild(newStyle);
+      chrome.runtime.onMessage.addListener(function (req, sender, res) {
+        // alert('test');
+        // console.log(document.styleSheets);
+
+        // document
+        //   .querySelectorAll('.card')
+        //   .forEach((element) => (element.style.backgroundColor = 'red'));
+        if (req === 'getCSS') {
+          const allCSS = [...document.styleSheets]
+            .map((styleSheet) => {
+              try {
+                return [...styleSheet.cssRules]
+                  .map((rule) => rule.cssText)
+                  .join('\n');
+              } catch (e) {
+                console.log(
+                  'Access to stylesheet %s is denied. Ignoring...',
+                  styleSheet.href
+                );
+              }
+            })
+            .filter(Boolean)
+            .join('\n');
+
+          cssObj.push(allCSS);
+          console.log('cssObj length is ' + cssObj.length);
+
+          res({ count: cssObj.length });
+        } else {
+          console.log('button clicked is: ' + req);
+          const style = '.card { background: red }';
+          document.querySelector('#toggle-toggle').textContent =
+            cssObj[req - 1];
+        }
+      });
+    }
+  }, 10);
 });
